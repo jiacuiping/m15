d40dbbcfb727ac0d2da468ae2f815c460e166620
jcp <1546732785@qq.com>
2020-02-29 17:50:55 +0800

    微信支付

--- application/api/controller/WechatPay.php
+++ application/api/controller/WechatPay.php
@@ -1,119 +1,38 @@
 <?php
 namespace app\api\controller;
-
-
-
-use NativePay;
-use WxPayUnifiedOrder;
-
-class WechatPay
+use think\Controller;
+class WechatPay extends Controller
 {
+    public function index(){
+        echo ROOT_PATH;die;
+        header("Content-type:text/html;charset=utf-8");
 
-    public function _initialize()
-    {
+        require ROOT_PATH . 'extend' . DS . 'wxpay/WxPay.Api.php'; //引入微信支付
+        $input = new \WxPayUnifiedOrder();//统一下单
+        $config = new \WxPayConfig();//配置参数
 
-    }
-    
-    //支付
-    public function payQrcode()
-    {
-        $notify = new NativePay();
-        $input = new WxPayUnifiedOrder();
-        $input->SetBody("test");
-        $input->SetAttach("test");
-        $input->SetOut_trade_no("sdkphp123456789".date("YmdHis"));
-        $input->SetTotal_fee("1");
+        //$paymoney = input('post.paymoney'); //支付金额
+        $paymoney = 1; //测试写死
+        $out_trade_no = 'WXPAY'.date("YmdHis"); //商户订单号(自定义)
+        $goods_name = '扫码支付'.$paymoney.'元'; //商品名称(自定义)
+        $input->SetBody($goods_name);
+        $input->SetAttach($goods_name);
+        $input->SetOut_trade_no($out_trade_no);
+        $input->SetTotal_fee($paymoney*100);//金额乘以100
         $input->SetTime_start(date("YmdHis"));
         $input->SetTime_expire(date("YmdHis", time() + 600));
         $input->SetGoods_tag("test");
-        $input->SetNotify_url("http://paysdk.weixin.qq.com/notify.php");
+        $input->SetNotify_url("http://www.xxx.com/wxpaynotify"); //回调地址
         $input->SetTrade_type("NATIVE");
-        $input->SetProduct_id("123456789");
+        $input->SetProduct_id("123456789");//商品id
+        $result = \WxPayApi::unifiedOrder($config, $input);
 
-        $result = $notify->GetPayUrl($input);
-        $url2 = $result["code_url"];
-        dump($url2);die;
+        if($result['result_code']=='SUCCESS' && $result['return_code']=='SUCCESS') {
+            $url = $result["code_url"];
+            $this->assign('url',$url);
+        }else{
+            $this->error('参数错误');
+        }
+        return view();
     }
-
-
-    //支付
-    public function PayNow($order_sn)
-    {
-        //获取订单详情
-        $order = $this->Order->GetOneData(array('order_sn'=>$order_sn));
-
-        //验证订单状态
-        if(!$order) return json_encode(array('code'=>0,'msg'=>'订单不存在'));
-        if($order['order_ispay'] == 1) return json_encode(array('code'=>0,'msg'=>'订单已支付'));
-        if($order['order_status'] > 9) return json_encode(array('code'=>0,'msg'=>'订单已支付'));
-        if(strtotime($order['order_time']) < time()-18000) return json_encode(array('code'=>0,'msg'=>'订单已失效'));
-
-        return $this->unifiedorder($order['order_money']*100,'商品购买',$this->User->GetField(array('user_id'=>$order['order_user']),'user_wxid'));
-    }
-
-    //统一下单
-	public function unifiedorder($total_fee=0, $body='', $openid='ofbua5ZvdCwNdxFnbmKuaIh6lW9k')
-    {
-        $data = [
-            'appid' => $this->appid,
-            'body' => $body,
-            'mch_id' => $this->mchid,
-            'nonce_str' => $this->nonce_str,
-            'notify_url' => $this->notify_url,
-            'openid' => $openid,
-            'out_trade_no' => time() . rand(11, 99),
-            'sign_type' => $this->sign_type,
-            'spbill_create_ip' => Request::instance()->ip(),
-            'total_fee' => $total_fee * 1,
-            'trade_type' => 'JSAPI',
-        ];
-
-        $data['sign'] = $this->GetSign($data);
-
-        $xml = $this->arrayToXml($data);
-
-        $result = $this->curl("https://api.mch.weixin.qq.com/pay/unifiedorder",$xml);
-
-        $return = $this->xmlToArray($result);
-
-        $this->package = 'prepay_id=' . $return['prepay_id'];
-        $this->renCreatesign();
-        $returns = [
-            'code'  => 1,
-            'appId' => $this->appid,
-            'timeStamp' => (string)$this->timestamp,
-            'nonceStr' => $this->nonce_str,
-            'package' => $this->package,
-            'signType' => $this->sign_type,
-            'paySign' => $this->resign,
-        ];
-
-        return json_encode($returns);
-    }
-
-    //二次签名
-    public function renCreatesign()
-    {
-        $build_data = [
-            'appId' => $this->appid,
-            'nonceStr' => $this->nonce_str,
-            'package' => $this->package,
-            'signType' => $this->sign_type,
-            'timeStamp' => $this->timestamp,
-            'key' => $this->key,
-        ];
-        $result = http_build_query($build_data);
-        $put_data = str_replace('%3D', '=', $result); //格式化网址
-        $signatrue = md5($put_data);
-
-        $this->resign = strtoupper($signatrue);
-
-    }
-
-    //小程序支付完成修改订单
-    public function CallBack($order_sn)
-    {
-
-    }
-
 }
